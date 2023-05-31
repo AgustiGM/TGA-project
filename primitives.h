9d828969aad95dcfce5048e982089e00ccf46981
Shuang <shuanglongjq@gmail.com>
2023-06-01 01:14:54 +0200

    backprop and update to main + functions to primitive

--- primitives.h
+++ primitives.h
@@ -5,3 +5,13 @@ __global__ void matMult(int N, int M, int P, float *A, float *B, float *C);
 __global__ void transpose(int N, int M, float *input, float *output);
 
 __global__ void globalSoftmaxPrimitive(int nOutput, int batchSize, float *input, float *output);
+
+__global__ void elementWiseProd(int N, int M, float *A, float *B, float *C);
+
+__global__ void subtractMat(int N, int M, float *A, float *B, float *C);
+
+__global__ void scalarDivMat(int N, int M, float value, float *A, float *C);
+
+__global__ void scalarProdMat(int N, int M, float value, float *A, float *C);
+
+__global__ int derivative(int N, int M, float *A, float *C);
